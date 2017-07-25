206ce4d6c9860ba99144ed8d3d8b56a8433e5c78
kazhappy7 <your.k-shimabuku@chanple.net>
2017-07-25 14:52:16 +0900

    行番号列が先頭に行くように修正

--- converter.rb
+++ converter.rb
@@ -3,10 +3,8 @@ require 'csv'
 data_list = 'sample.csv'
 file_path = 'index_db.csv'
 
-csv_table = CSV.table(data_list, header_converters: lambda { |h| h.to_s } )
+csv_table = CSV.table(data_list, header_converters: lambda {|h| h == 'コード' ? '行番号' : h})
 csv_table.map.with_index(1) {|row, i| row['行番号'] = i}
-csv_table.delete('コード')
 csv_table.delete('番地')
-#puts csv_table
 
 File.open(file_path, 'wb'){|f| f.puts(csv_table.to_csv)}
